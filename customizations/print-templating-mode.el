b15fe5378947f6c2ca1b2912399c8fc385490ace
gixxi <christian.meichsner@live.com>
2024-05-02 15:46:13 +0200

    Draft for print-templating-mode

---
+++ customizations/print-templating-mode.el
@@ -0,0 +1,73 @@
+;; customizations/print-templating-mode.el
+
+(require 'cider)
+
+(defvar-local print-templating-datatype nil
+  "Datatype to use for the print templating evaluation.")
+
+(defvar-local print-templating-id nil
+  "ID to use for the print templating evaluation.")
+
+
+(with-eval-after-load 'cider
+
+
+(defun evaluate-clojure-function-and-display ()
+  "Evaluate a predefined Clojure function using the buffer's content as the template and display the results in another buffer."
+  (interactive)
+  (let ((code (format "(do
+                        (require 
+                         '[lambdaroyal.memory.core.tx :as tx]
+                         '[lambdaroyal.vlic.ioc :refer :all]
+                         '[lambdaroyal.vlic.crosscutting.print-templating :as pt])
+                        (with-tx @lambdaroyal.vlic.main/system
+                          (let [datatype (keyword \"%s\")
+                                id %s
+                                template \"%s\"]
+                            (let [record (tx/select-first *tx* datatype id)]
+                              (binding [pt/*x* record
+                                        pt/*escape-character-lambda* pt/escape-latex-characters]
+                                (pt/run* (pt/read* template)))))))"
+                        print-templating-datatype
+                        print-templating-id
+                        (replace-regexp-in-string "\"" "\\\\\""
+                                                  (buffer-string)))))
+    (cider-interactive-eval code
+                            (nrepl-make-response-handler (current-buffer)
+                                                         (lambda (buffer value)
+                                                           (with-current-buffer (get-buffer-create "*Clojure Output*")
+                                                             (read-only-mode 0)
+                                                             (erase-buffer)
+                                                             (insert value)
+                                                             (read-only-mode 1)
+                                                             (display-buffer (current-buffer))))
+                                                         nil nil nil))))
+
+  (define-minor-mode print-templating-mode
+    "A minor mode for evaluating Clojure templates on buffer save."
+    :lighter " PT"
+    :global nil
+    (if print-templating-mode
+        (print-templating-enable)
+      (print-templating-disable)))
+
+  (defun print-templating-enable ()
+    "Enable print templating mode, asking user for datatype and ID, and evaluate immediately."
+    (interactive)
+    (setq print-templating-datatype (read-string "Enter datatype: "))
+    (setq print-templating-id (read-string "Enter ID: "))
+    (add-hook 'after-save-hook #'evaluate-clojure-function-and-display nil t)
+    (evaluate-clojure-function-and-display)  ; Evaluate immediately after setup
+    (message "Print Templating mode enabled and evaluated."))
+
+
+  (defun print-templating-disable ()
+    "Disable print templating mode."
+    (interactive)
+    (remove-hook 'after-save-hook #'evaluate-clojure-function-and-display t)
+    (message "Print Templating mode disabled."))
+
+
+  ;; Define your enabling and disabling functions here as well.
+  )
+
